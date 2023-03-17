5f08b6be976b1a4a88637ad698bc910cb3302611
Tunahan Yıldız <tunahanyildiz@outlook.it>
2023-03-17 16:25:12 +0300

    Inversion Of Control Principle implemented

--- IOC.console/BL.cs
+++ IOC.console/BL.cs
@@ -8,16 +8,12 @@ namespace IOC.console
 {
     public class BL
     {
-        //Bl içinde başka classlar newledik ve bu da 
-        //bir bağımlılık oluşturdu.
-        //DAL metotlarından herhangı bırısının ısmı degısırse veya
-        //bir degısıklık olursa bu buradakı elemanlarına da yansıyacak
-        //ve bunu ıstemıyoruz. Burada tightly coupled bir kod var
         private DAL _dal { get; set; }
 
         public BL()
         {
-            _dal = new DAL();
+            //Bl Dal kısmını artık dısarıdan alacak
+            _dal = DALFactory.GetDAL();
         }
         public List<Product> GetProducts()
         {
