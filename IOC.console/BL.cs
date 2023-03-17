ea3dbbcd60c44ef16b84b88687ddf64e2cba7e10
Tunahan Yıldız <tunahanyildiz@outlook.it>
2023-03-17 16:19:10 +0300

    Add project files.

---
+++ IOC.console/BL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOC.console
+{
+    public class BL
+    {
+        //Bl içinde başka classlar newledik ve bu da 
+        //bir bağımlılık oluşturdu.
+        //DAL metotlarından herhangı bırısının ısmı degısırse veya
+        //bir degısıklık olursa bu buradakı elemanlarına da yansıyacak
+        //ve bunu ıstemıyoruz. Burada tightly coupled bir kod var
+        private DAL _dal { get; set; }
+
+        public BL()
+        {
+            _dal = new DAL();
+        }
+        public List<Product> GetProducts()
+        {
+            return _dal.GetProducts();
+        }
+    }
+}
---
+++ IOC.console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IOC.console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BL bl = new BL();
+            bl.GetProducts().ForEach(x =>
+           {
+               Console.WriteLine(x.Id);
+               Console.WriteLine(x.Name);
+               Console.WriteLine(x.Price);
+               Console.WriteLine(x.Stock);
+               Console.WriteLine("--------------");
+           });
+        }
+    }
+}
